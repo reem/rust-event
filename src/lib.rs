741ba64f653c82fcf8bc10ea32a921dc113a98e8
Jonathan Reem <jonathan.reem@gmail.com>
2014-09-16 03:48:06 -0700

    (doc) Crate summary.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,48 @@
 //#![deny(missing_doc)]
 #![deny(warnings)]
 
-//! Crate comment goes here
+//! A multi-threaded Event-Loop for Rust.
+//!
+//! This event loop supports only events which originate from Rust, but can be used
+//! as a global event queue for inter-task and asynchronous communication.
+//!
+//! Rust-Event exports a small and focused API:
+//!
+//! ```{rust, ignore}
+//! fn queue() -> Ref<Arc<EventQueue>>;
+//!
+//! struct Event<T>;
+//!
+//! impl<T: Send> Event<T> {
+//!   fn new(val: T) -> Event<T>;
+//!   fn trigger<K: Assoc<T>>(self);
+//! }
+//!
+//! type Handler<T> = Box<Fn<(Box<Event<T>>,), ()> + Send;
+//!
+//! fn on<K: Assoc<T>, T: Send>(handler: Handler<T>);
+//!
+//! fn dedicate();
+//!
+//! impl EventQueue {
+//!   fn trigger(&self);
+//! }
+//!
+//! fn spawn(proc(): Send);
+//! ```
+//!
+//! It has one method for triggering events - `Event::trigger`, and one
+//! method for listening for events `on`. Threads can offer themselves
+//! to be used to continuously handle events using `dedicate`, which will
+//! block the thread, causing it to spend all of its time consuming events
+//! from the global EventQueue.
+//!
+//! Threads can try to handle a single event through `EventQueue::trigger`.
+//!
+//! `spawn` is used to spawn tasks which have access to the `EventQueue`.
+//!
+//! That's it! Further abstractions should be built above this basic API.
+//!
 
 extern crate typemap;
 extern crate "unsafe-any" as uany;
