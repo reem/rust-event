4f1b0461d525fd4dc643663f9ec377c4f862f84e
Jonathan Reem <jonathan.reem@gmail.com>
2014-12-31 10:18:31 -0800

    (feat) Update examples to use ClosureHandler.

--- examples/tcp.rs
+++ examples/tcp.rs
@@ -6,99 +6,58 @@ extern crate mio;
 #[phase(plugin, link)]
 extern crate log;
 
-use event::{run, register, Handler};
+use event::{run, register, ClosureHandler};
 
 use mio::net::SockAddr;
 use mio::net::tcp::{TcpSocket, TcpAcceptor};
-use mio::{IoHandle, IoReader, IoWriter, IoAcceptor, IoDesc};
+use mio::{IoReader, IoWriter, IoAcceptor};
 use mio::event as evt;
 
 const RESPONSE: &'static str = "HTTP/1.1 200 OK\r
-Content-Length: 11\r
+Content-Length: 14\r
 \r
 Hello World\r
 \r";
 
-struct TcpAcceptHandler {
-    acceptor: TcpAcceptor
-}
-
-struct TcpConnHandler {
-    sock: TcpSocket,
-    message: &'static [u8],
-    readbuf: [u8, ..64 * 1024]
-}
-
-impl Handler for TcpAcceptHandler {
-    fn readable(&mut self, _: evt::ReadHint) -> bool {
-        info!("Accepting a new conncetion.");
-        self.accept();
-        true
-    }
-
-    // Can't happen, no-op
-    fn writable(&mut self) -> bool { true }
-
-    fn desc(&self) -> &IoDesc {
-        info!("Registering an acceptor.");
-        self.acceptor.desc()
-    }
-}
-
-impl TcpAcceptHandler {
-    fn accept(&mut self) {
-        let sock = self.acceptor.accept().unwrap();
-
-        if !sock.would_block() {
-            let conn = TcpConnHandler {
-                sock: sock.unwrap(),
-                message: RESPONSE.as_bytes(),
-                readbuf: [0, ..64 * 1024]
-            };
-            register(conn);
+fn read_socket(sock: &mut TcpSocket, _: evt::ReadHint) -> bool {
+    info!("Reading from a socket.");
+    match sock.read_slice(&mut [0, ..1024]) {
+        Ok(_) => { true }
+        Err(ref e) if e.is_eof() => false,
+        Err(e) => {
+            error!("Error reading: {}", e);
+            false
         }
     }
 }
 
-impl Handler for TcpConnHandler {
-    fn readable(&mut self, _: evt::ReadHint) -> bool {
-        info!("Reading from an existing tcp connection.");
-
-        match self.sock.read_slice(&mut self.readbuf) {
-            Ok(_) => { true }
-            Err(ref e) if e.is_eof() => false,
-            Err(e) => {
-                error!("Error reading: {}", e);
-                false
-            }
+fn write_socket(sock: &mut TcpSocket) -> bool {
+    info!("Writing to a socket.");
+    match sock.write_slice(RESPONSE.as_bytes()) {
+        Ok(_) => { true }
+        Err(ref e) if e.is_eof() => false,
+        Err(e) => {
+            error!("Error writing: {}", e);
+            false
         }
     }
+}
 
-    fn writable(&mut self) -> bool {
-        info!("Writing to an existing tcp connection.");
+fn accept(acceptor: &mut TcpAcceptor, _: evt::ReadHint) -> bool {
+    info!("Accepting a connection.");
+    let sock = acceptor.accept().unwrap();
 
-        match self.sock.write_slice(self.message) {
-           Ok(wrote) => {
-               self.message = self.message.slice_from(wrote.unwrap());
-               true
-           },
-           Err(ref e) if e.is_eof() => false,
-           Err(e) => {
-               error!("Error writing: {}", e);
-               false
-           }
-        }
+    if !sock.would_block() {
+        register(ClosureHandler {
+            io: sock.unwrap(),
+            read: read_socket,
+            write: write_socket,
+            interest: Some(evt::READABLE | evt::WRITABLE),
+            opt: Some(evt::PollOpt::edge())
+        });
     }
 
-    fn desc(&self) -> &IoDesc { self.sock.desc() }
-
-    fn interest(&self) -> Option<evt::Interest> {
-        Some(evt::READABLE | evt::WRITABLE)
-    }
-
-    fn opt(&self) -> Option<evt::PollOpt> {
-        Some(evt::PollOpt::edge())
-    }
+    true
 }
 
 fn main() {
@@ -110,7 +69,13 @@ fn main() {
         .bind(&addr).unwrap()
         .listen(256u).unwrap();
 
-    register(TcpAcceptHandler { acceptor: srv });
+    register(ClosureHandler {
+        io: srv,
+        read: accept,
+        write: move |&mut: _: &mut TcpAcceptor| true,
+        interest: Some(evt::READABLE),
+        opt: Some(evt::PollOpt::edge())
+    });
     run();
 }
 
