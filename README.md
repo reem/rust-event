e092dbf729dcd0a68a13cd3f89c776069f117edf
Jonathan Reem <jonathan.reem@gmail.com>
2015-02-15 17:41:14 -0800

    (opt) Optimize tcp example to get over 890k req/sec.

--- README.md
+++ README.md
@@ -24,16 +24,15 @@ $ wrk -t12 -c100 -d10s http://localhost:3000/
 Running 10s test @ http://localhost:3000/
   12 threads and 100 connections
   Thread Stats   Avg      Stdev     Max   +/- Stdev
-    Latency     0.90ms  233.11us   2.33ms   88.39%
-    Req/Sec    42.29k     4.51k   52.89k    73.75%
-  4825633 requests in 10.00s, 243.91MB read
-Requests/sec: 482629.32
-Transfer/sec:     24.39MB
+    Latency     0.00us    0.00us   0.00us  100.00%
+    Req/Sec     0.94M    73.98k    1.03M    90.09%
+  8913835 requests in 10.01s, 450.55MB read
+  Socket errors: connect 0, read 0, write 0, timeout 397
+Requests/sec: 890692.14
+Transfer/sec:     45.02MB
 ```
 
-Ya, it's fast.
-
-\* My machine only, YMMV
+You did read that correctly.
 
 ## License
 
--- examples/tcp.rs
+++ examples/tcp.rs
@@ -9,7 +9,7 @@ use event::{run, register, ClosureHandler};
 
 use mio::net::SockAddr;
 use mio::net::tcp::{TcpSocket, TcpAcceptor};
-use mio::{IoReader, IoWriter, IoAcceptor};
+use mio::{IoWriter, IoAcceptor};
 use mio::event as evt;
 
 const RESPONSE: &'static str = "HTTP/1.1 200 OK\r
@@ -18,41 +18,23 @@ Content-Length: 14\r
 Hello World\r
 \r";
 
-fn read_socket(sock: &mut TcpSocket, _: evt::ReadHint) -> bool {
-    info!("Reading from a socket.");
-    match sock.read_slice(&mut [0; 1024]) {
-        Ok(_) => { true }
-        Err(ref e) if e.is_eof() => false,
-        Err(e) => {
-            error!("Error reading: {:?}", e);
-            false
-        }
-    }
-}
-
-fn write_socket(sock: &mut TcpSocket) -> bool {
-    info!("Writing to a socket.");
-    match sock.write_slice(RESPONSE.as_bytes()) {
-        Ok(_) => { true }
-        Err(ref e) if e.is_eof() => false,
-        Err(e) => {
-            error!("Error writing: {:?}", e);
-            false
-        }
-    }
-}
-
 fn accept(acceptor: &mut TcpAcceptor, _: evt::ReadHint) -> bool {
-    info!("Accepting a connection.");
     let sock = acceptor.accept().unwrap();
 
     if !sock.would_block() {
         register(ClosureHandler {
             io: sock.unwrap(),
-            read: read_socket,
-            write: write_socket,
-            interest: Some(evt::READABLE | evt::WRITABLE),
-            opt: Some(evt::PollOpt::edge())
+            read: |_: &mut _, _| true,
+            write: |sock: &mut TcpSocket| {
+                loop {
+                    match sock.write_slice(RESPONSE.as_bytes()) {
+                        Ok(..) => {},
+                        Err(..) => return false
+                    }
+                }
+            },
+            interest: Some(evt::WRITABLE),
+            opt: Some(evt::PollOpt::level())
         }).unwrap();
     }
 
